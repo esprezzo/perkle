826a133e973b06c88e5ad60eb771dbb5de27cba7
Alan <x>
2020-01-31 10:03:07 -0800

    CI@1580427604

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 ./build/bin/geth \
     --datadir ~/.perkle \
-	  --allow-insecure-unlock \
     --syncmode 'full' \
     --rpc \
     --rpcaddr '0.0.0.0' \
@@ -9,7 +8,9 @@
     --rpcapi 'eth,net,web3,txpool,miner' \
     --verbosity 3 \
     --rpccorsdomain="*" \
+    --nousb \
     --rpcvhosts=*
     # --mine \ # The following commands are useful for miners
     # --minerthreads 0 \
     # --etherbase '0x0f31986d7a0d4f160acd97583e3c3b591dcb5dde' \
+    # --allow-insecure-unlock \
