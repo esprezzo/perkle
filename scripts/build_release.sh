0641ef1b72da3ad4df8d4783e0e90c3ed38c52f2
Alan <x>
2020-02-03 13:58:46 -0800

    CI@1580497024

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -3,9 +3,9 @@
 rm -rf ~/_build_tmp;
 mkdir ~/_build_tmp;
 
-sed -i '\|PATH="$PATH:/usr/lib/go-1.10/bin"|d' ~/.profile;
-echo 'PATH="$PATH:/usr/lib/go-1.10/bin"' >> ~/.profile;
-source ~/.profile
+# sed -i '\|PATH="$PATH:/usr/lib/go-1.10/bin"|d' ~/.profile;
+# echo 'PATH="$PATH:/usr/lib/go-1.10/bin"' >> ~/.profile;
+# source ~/.profile
 
 cd ~/_build_tmp && \
 git clone https://github.com/esprezzo/perkle.git 
---
+++ scripts/start_node_dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+./build/bin/perkle \
+    --datadir ~/.perkle \
+    --syncmode 'full' \
+    --verbosity 3 \
+    --rpccorsdomain="*" \
+    --nousb \
+    --rpcvhosts=* \
+    --rpc \
+    --rpcaddr '0.0.0.0' \
+    --rpcport 8501 \
+    --allow-insecure-unlock \
+    --rpcapi 'debug,eth,net,web3,txpool' 
+    # --mine \ # The following commands are useful for miners
+    # --minerthreads 0 \
+    # --etherbase '0x0f31986d7a0d4f160acd97583e3c3b591dcb5dde' \
