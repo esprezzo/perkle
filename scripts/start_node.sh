6c8cdeb3a48394ab7b014f332a20da467eedc19e
alanwilhelm <ajwilhelm@protonmail.com>
2022-11-21 16:59:43 -0800

    ...

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 ./build/bin/perkle \
-    --datadir ~/.perkle \
     --syncmode 'full' \
+    --rpcapi 'eth,net,web3' \
+    --gcmode archive \
     --verbosity 3 \
     --rpccorsdomain="*" \
     --nousb \
+    --rpcaddr '0.0.0.0' \
+    --rpcport 8501 \
     --rpcvhosts=*
-    
-    # ETC.
-    # --rpc \
-    # --rpcaddr '0.0.0.0' \
-    # --rpcport 8501 \
-    # --rpcapi 'eth,net,web3,txpool' \
-    # --mine \ # The following commands are useful for miners
-    # --minerthreads 0 \
-    # --etherbase '0x0f31986d7a0d4f160acd97583e3c3b591dcb5dde' \
-    # --allow-insecure-unlock \
